1e77583feeddb9cf2a7380c4499bc004a2e2064e
NorSAUCE <you@example.com>
2022-09-25 16:20:01 -0300

    schema pets simple finish

--- back/src/schemas/Pets.ts
+++ back/src/schemas/Pets.ts
@@ -1,16 +1,40 @@
-import mongoose, {Document, Schema} from 'mongoose';
+import mongoose, {Document, Schema, Types} from 'mongoose';
 
 export interface Pets {
     name:string,
-
+    description:string,
+    species:Types.ObjectId,
+    sex:string,
+    size:string,
+    race:Types.ObjectId,
+    age:number,
+    color:string,
+    location:number,
+    state:string,
+    date:string,
+    image:string,
+    observation:string,
 }
 
 export interface PetsModel extends Pets, Document {} 
 
 const PetsSchema : Schema = new Schema (
     {
-        name:{type:String, required:true}
-
+        name:{type:String, required:true},
+        description:{type:String},
+        species:{type: Schema.Types.ObjectId, ref: 'Species'},
+        sex:{type:String},
+        size:{type:String},
+        race:{type: Schema.Types.ObjectId, ref: 'Race'},
+        age:{type:Number},
+        color:{type:String},
+        location:{type:Number},
+        state:{type:String},
+        date:{type:String},
+        image:{type:String},
+        observation:{type:String},
     },
 )
+
+
 export default mongoose.model<PetsModel>('Pets',PetsSchema);
