03b743c4c0b2b50644900187919fcb8778e700d5
Jeyther <jeyther13@gmail.com>
2022-09-26 17:34:10 -0500

    hero created

--- front/src/pages/home/Home.tsx
+++ front/src/pages/home/Home.tsx
@@ -1,12 +1,13 @@
-import { Button } from '@mui/material'
+import { Button, Stack } from '@mui/material'
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Hero from '../../components/home/Hero'
 
 const Home = () => {
     return (
-        <div>
-            Home
-        </div>
+        <Stack width='100%'>
+            <Hero/>
+        </Stack>
     )
 }
 
