be09fe993defebfb9dba1d9aacc2291aac03bb11
Bellantra <yamilapaez2@hotmail.com>
2022-09-26 20:08:37 +0200

    router working

--- back/src/schemas/User.ts
+++ back/src/schemas/User.ts
@@ -25,7 +25,7 @@ const userSchema = new Schema<User>({
     pets: [
         {
             types: Types.ObjectId,
-            ref: 'User',
+            //ref: 'Pet',
         },
     ],
     admin: {
