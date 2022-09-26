60c28b207109d72451e05ed6fd1be0f51fefe5bd
Francisco Rey <reyf1998@outlook.es>
2022-09-26 11:00:03 -0300

    Installation of react router dom and test

--- front/src/pages/home/Home.tsx
+++ front/src/pages/home/Home.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <div>
+            Home
+            <Link to="/found">Testeando router</Link>
+        </div>
+    )
 }
 
 export default Home
