472e4741f9d159d0d25c52f2e4e77e2c52837be3
Bellantra <yamilapaez2@hotmail.com>
2022-09-27 02:50:12 +0200

    validateResults added

--- back/src/utils/checks/signUpCheck.ts
+++ back/src/utils/checks/signUpCheck.ts
@@ -7,7 +7,7 @@ export const signUpCheck = () => {
             .withMessage('Email field is required')
             .isEmail()
             .withMessage('Please enter a valid email address'),
-        body('password', 'El password debe de ser de 6 caracteres')
+        body('password')
             .isStrongPassword()
             .withMessage(
                 'The Password has to contain min 8 chars, 1 lowercase(min), 1 uppercase(min), 1 number(min), 1 symbol(min) '
