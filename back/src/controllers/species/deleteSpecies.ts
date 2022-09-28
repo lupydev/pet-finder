d4b585e2b4c2844610bee4b34f86bc272e46d911
NorSAUCE <you@example.com>
2022-09-28 16:43:18 -0300

    fix species

--- back/src/controllers/species/deleteSpecies.ts
+++ back/src/controllers/species/deleteSpecies.ts
@@ -2,21 +2,21 @@ import Species from "../../schemas/Species";
 import { Request, Response } from 'express'
 
 const deleteSpecies = async (req: Request, res: Response) => {
-      const {id} = req.params
+    const {id} = req.params
 
-      try {
+    try {
 
-            const species = await Species.deleteOne({_id: { $eq: id}});
-            res.status(200).json({ species, ok: true, msg: 'Species deleted' })
+        const species = await Species.deleteOne({_id: { $eq: id}});
+        res.status(200).json({ species, ok: true, msg: 'Species deleted' })
 
-      } catch (error) {
+    } catch (error) {
 
-            console.log(error)
-            res.status(404).json({
-                  ok: false,
-                  msg: 'An error occured, contact with admin',
-            })
+        console.log(error)
+        res.status(404).json({
+            ok: false,
+            msg: 'An error occured, contact with admin',
+        })
             
-      }
+    }
 }
 export default deleteSpecies
