a2601e727c14ec6aeb0de38add4fe9432ded57b3
NorSAUCE <you@example.com>
2022-09-28 16:00:19 -0300

    species routes and controllers

--- back/src/routes/index.ts
+++ back/src/routes/index.ts
@@ -1,5 +1,6 @@
 import userRoutes from './userRoutes'
 import petRoutes from './petRoutes'
+import speciesRoutes from './speciesRoutes'
 import express from 'express'
 
 const router = express.Router()
@@ -7,5 +8,7 @@ const router = express.Router()
 router.use('/users', userRoutes)
 
 router.use('/pets',petRoutes)
+
+router.use('/species',speciesRoutes)
 
 export { router }
