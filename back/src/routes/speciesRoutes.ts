bded1bd0b1d0018a83a3a4c4bebf888f6a7d1e1d
NorSAUCE <you@example.com>
2022-09-28 16:36:00 -0300

    fixed species routes

--- back/src/routes/speciesRoutes.ts
+++ back/src/routes/speciesRoutes.ts
@@ -1,10 +1,10 @@
 import express from 'express'
 import createSpecies from '../controllers/species/createSpecies'
 import deleteSpecies from '../controllers/species/deleteSpecies'
-import {Request, Response} from 'express'
+
 const router = express.Router()
 
-router.get('/hola', async(_req:Request,res:Response) => {res.send("Hola")})
+
 
 router.post('/new', createSpecies)
 
