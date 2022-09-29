3eb1cc5824da0cb44d23f2c1820ca5e655d9ba9b
NorSAUCE <you@example.com>
2022-09-29 11:34:44 -0300

    fix delete pet

--- back/src/controllers/pets/deletePet.ts
+++ back/src/controllers/pets/deletePet.ts
@@ -4,24 +4,24 @@ import { Request, Response } from 'express'
 const deletePet = async (req: Request, res: Response) => {
     const { id } = req.params
 
-      try {
+    try {
 
-            const pet = await Pet.findByIdAndDelete({_id: id});
+        const pet = await Pet.findByIdAndDelete({_id: id});
 
-            if(pet){               
-                  return res.status(200).json({ pet, ok: true, msg: 'Pet deleted' })
-            }else{
-                  return res.status(404).json({ ok: false, msg: 'Pet id not found' })
-            }
+        if(pet){               
+            return res.status(200).json({ pet, ok: true, msg: 'Pet deleted' })
+        }else{
+            return res.status(404).json({ ok: false, msg: 'Pet id not found' })
+        }
 
-      } catch (error) {
+    } catch (error) {
 
-            console.log(error)
-            return res.status(404).json({
-                  ok: false,
-                  msg: 'An error occurred, contact with admin',
-            })
+        console.log(error)
+        return res.status(404).json({
+            ok: false,
+            msg: 'An error occurred, contact with admin',
+        })
             
-      }
+    }
 }
 export default deletePet
