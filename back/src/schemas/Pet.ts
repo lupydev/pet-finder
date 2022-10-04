3433e12071e698a512aad4b9dd544bc403b57c4e
Bellantra <yamilapaez2@hotmail.com>
2022-10-04 16:31:38 +0200

    change to Pet schema color

--- back/src/schemas/Pet.ts
+++ back/src/schemas/Pet.ts
@@ -2,19 +2,30 @@ import { model, Schema } from 'mongoose'
 import { Pets } from '../interfaces/pets'
 
 const PetsSchema = new Schema({
-    name: { type: String, default:'No Name' },
-    userId: { type: Schema.Types.ObjectId, ref: 'User', required:true },
-    description: { type: String, default:'Withouth description' },
-    species: { type: Schema.Types.ObjectId, ref: 'Species',required:true},
-    gender: { type: String, enum: ['Male', 'Female','Unknown'],required:true ,default:'Unknown'},
-    size: { type: String, enum: ['Small', 'Medium', 'Large'],required:true },
+    name: { type: String, default: 'No Name' },
+    userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    description: { type: String, default: 'Withouth description' },
+    species: { type: Schema.Types.ObjectId, ref: 'Species', required: true },
+    gender: {
+        type: String,
+        enum: ['Male', 'Female', 'Unknown'],
+        required: true,
+        default: 'Unknown',
+    },
+    size: { type: String, enum: ['Small', 'Medium', 'Large'], required: true },
     type: { type: String, enum: ['Lost', 'Found'], required: true },
-    breed: { type: Schema.Types.ObjectId, ref: 'Breed',required:true },
+    breed: { type: Schema.Types.ObjectId, ref: 'Breed', required: true },
     age: { type: Number },
-    color: { type: String, enum: ['White','Black','Brown','LightBrown', 'Grey'],required:true },
+    color: [
+        {
+            type: String,
+            enum: ['White', 'Black', 'Brown', 'LightBrown', 'Grey'],
+            required: true,
+        },
+    ],
     location: { type: String },
     status: { type: String, default: 'Active' },
-    date: { type: Date, required:true },
+    date: { type: Date, required: true },
     img: [
         {
             type: String,
