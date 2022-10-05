6fed4563c1e932ab73e4e6debd06d5adf5301c19
Francisco Rey <reyf1998@outlook.es>
2022-10-05 16:57:14 -0300

    styles profile info

--- front/src/pages/profile/ProfileDetail.jsx
+++ front/src/pages/profile/ProfileDetail.jsx
@@ -1,13 +1,83 @@
+import {
+    Container,
+    Paper,
+    Box,
+    Typography,
+    Divider,
+    Avatar,
+    IconButton,
+} from '@mui/material'
+
 const ProfileDetail = ({ userData }) => {
     return (
-        <div>
-            <ul>
-                <div key={userData?._id}>
-                    <h3>{userData?.nickname}</h3>
-                    <h3>{userData?.email}</h3>
-                    <img style={{ width: '200px' }} src={userData?.img} />
-                </div>
-            </ul>
+        <div key={userData?._id}>
+            <Container maxWidth="sm">
+                <Paper
+                    elevation={6}
+                    sx={{
+                        backgroundColor: 'primary.main',
+                    }}
+                >
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Box
+                            display="flex"
+                            justifyContent="flex-end"
+                            width="100%"
+                        >
+                            <IconButton
+                                color="primary"
+                                aria-label="edit"
+                            ></IconButton>
+                        </Box>
+                        <Typography variant="h4" marginY={5} color="white">
+                            My Profile
+                        </Typography>
+
+                        <Divider variante="middle"></Divider>
+
+                        <Avatar
+                            sx={{ width: 200, height: 200 }}
+                            src={userData?.img}
+                            alt={userData?.nickname}
+                        />
+
+                        <Typography
+                            sx={{
+                                p: 1.5,
+                                borderRadius: 3,
+                                bgcolor: 'white',
+                            }}
+                            border="1px solid gray"
+                            variant="body1"
+                            color="text.primary"
+                            marginTop={5}
+                        >
+                            <b> Name:</b> {userData?.fullName}
+                        </Typography>
+
+                        <Typography
+                            sx={{
+                                p: 1.5,
+                                borderRadius: 3,
+                                bgcolor: 'white',
+                            }}
+                            border="1px solid gray"
+                            variant="body1"
+                            color="text.primary"
+                            marginTop={5}
+                            marginBottom={5}
+                        >
+                            <b> Email:</b> {userData?.email}
+                        </Typography>
+                    </Box>
+                </Paper>
+            </Container>
         </div>
     )
 }
