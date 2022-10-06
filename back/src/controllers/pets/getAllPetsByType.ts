1ae892aa56b310f737e0b2d76be683782e4810ef
Bellantra <yamilapaez2@hotmail.com>
2022-10-06 22:48:26 +0200

    order by name working

--- back/src/controllers/pets/getAllPetsByType.ts
+++ back/src/controllers/pets/getAllPetsByType.ts
@@ -3,18 +3,28 @@ import { Request, Response } from 'express'
 
 const getAllPets = async (req: Request, res: Response) => {
     const { type } = req.params
-    const {color,gender,species,breed} = req.query
+    const { color, gender, species, breed, name } = req.query
+
+    console.log(name)
     try {
         const pets = await Pet.find({
             $and: [{ type: { $eq: type } }, { status: 'Active' }],
         })
-            .and([color ? { color: { $regex: `^${color}`, $options: 'i' } } : {}])
-            .and([gender ? { gender: { $regex: `^${gender}`, $options: 'i' } } : {}])
-            .and([species ? { species: { $eq:species } } : {}])
-            .and([breed ? { breed: { $eq:breed } } : {}])
+            .and([
+                color ? { color: { $regex: `^${color}`, $options: 'i' } } : {},
+            ])
+            .and([
+                gender
+                    ? { gender: { $regex: `^${gender}`, $options: 'i' } }
+                    : {},
+            ])
+            .and([species ? { species: { $eq: species } } : {}])
+            .and([breed ? { breed: { $eq: breed } } : {}])
             .populate({ path: 'breed', select: '_id name' })
             .populate({ path: 'species', select: '_id name' })
-        
+            .sort(name === 'asc' ? { name: 1 } : {})
+            .sort(name === 'desc' ? { name: -1 } : {})
+
         if (pets.length > 0) {
             return res
                 .status(200)
