d39a13b81cec4251ac4189cdfb6b369a80df8df5
Bellantra <yamilapaez2@hotmail.com>
2022-10-09 14:27:07 +0200

    pet routes

--- back/src/routes/index.ts
+++ back/src/routes/index.ts
@@ -1,7 +1,20 @@
 import userRoutes from './userRoutes'
+import petRoutes from './petRoutes'
+import speciesRoutes from './speciesRoutes'
+import authRoutes from './authRoutes'
+import breedsRoutes from './breedsRoutes'
 import express from 'express'
+
 const router = express.Router()
 
 router.use('/users', userRoutes)
+
+router.use('/pets', petRoutes)
+
+router.use('/species', speciesRoutes)
+
+router.use('/', authRoutes)
+
+router.use('/breeds', breedsRoutes)
 
 export { router }
