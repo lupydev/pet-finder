b3ed0f2e1687b729e5f3557ffdf9927f704fd876
Bellantra <yamilapaez2@hotmail.com>
2022-10-10 23:24:49 +0200

    tercer petslice corregido

--- front/src/pages/petBrowser/petBrowserContainer.jsx
+++ front/src/pages/petBrowser/petBrowserContainer.jsx
@@ -1,11 +1,11 @@
 import { Stack } from '@mui/material'
 import React from 'react'
-import PetBrowser from '../../components/petBrowser/PetBrowser'
+import PetBrowser from '../../components/petBrowser/petBrowser'
 
 const petBrowserContainer = (props) => {
     return (
-        <Stack alignItems="center" width={'100%'}  gap="25px">
-            <PetBrowser  title={props.title} color={props.color} />
+        <Stack alignItems="center" width={'100%'} gap="25px">
+            <PetBrowser title={props.title} color={props.color} />
         </Stack>
     )
 }
