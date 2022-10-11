41d2931e9b45c2b88d80bc7a79e32db8f73beeaa
NorSAUCE <you@example.com>
2022-10-11 16:05:11 -0300

    secure routes

--- back/src/routes/breedsRoutes.ts
+++ back/src/routes/breedsRoutes.ts
@@ -3,13 +3,14 @@ import express from 'express';
 import deleteBreed from '../controllers/breeds/deleteBreed'
 import createBreed from '../controllers/breeds/createBreed'
 import getAllBreedsBySpecies from '../controllers/breeds/getAllBreedsBySpecies'
+import { validateAdmin } from '../middlewares/validateAdmin';
 
 const router = express.Router();
 
 router.get('/getBreed/:species', getAllBreedsBySpecies)
 
-router.post('/newBreed', createBreed)
+router.post('/newBreed',validateAdmin, createBreed)
 
-router.delete('/delete/:id', deleteBreed)
+router.delete('/delete/:id',validateAdmin, deleteBreed)
 
 export default router
